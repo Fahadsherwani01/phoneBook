18d54eafb05e9580216d867478b0172573f7967e
Fahad Sherwani <fahadsherwani01@outlook.com>
2018-10-08 14:46:35 +0500

    firstcommit

--- WebApplication7/Models/ManageViewModels.cs
+++ WebApplication7/Models/ManageViewModels.cs
@@ -8,7 +8,7 @@ namespace WebApplication7.Models
     public class IndexViewModel
     {
         public bool HasPassword { get; set; }
-        public IList<UserLoginInfo> Logins { get; set; }
+        public IList<UserLoginInfo> Logins { get; set; } 
         public string PhoneNumber { get; set; }
         public bool TwoFactor { get; set; }
         public bool BrowserRemembered { get; set; }
